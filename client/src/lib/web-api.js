a70065693c0bb1115e78922ce2ed85b44a591e02
Kbigbean <48699529+Kbigbean@users.noreply.github.com>
2019-12-22 14:48:21 +0900

    end

---
+++ client/src/lib/web-api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const createMemo = ({title, body}) => axios.post('/memo', {title,body});
+export const getInitialMemo = () => axios.get('/memo/?_sort=id&_order=DESC&_limit=20');
+export const getRecentMemo = (cursor) => axios.get(`/memo/?id_gte=${cursor+1}&_sort=id&_order=DESC&`);
+export const updateMemo = ({id, memo: { title, body }}) => axios.put(`/memo/${id}`, {title, body}); // 메모를 업데이트한다
+export const deleteMemo = (id) => axios.delete(`/memo/${id}`); // 메모를 제거한다
+export const getPreviousMemo = (endCursor) => axios.get(`/memo/?_sort=id&_order=DESC&_limit=20&id_lte=${endCursor-1}`);
